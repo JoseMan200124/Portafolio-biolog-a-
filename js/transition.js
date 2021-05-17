420edcfe755e41a4f754207274d5d1f784abf2b4
José Man <34019072+JoseMan200124@users.noreply.github.com>
2021-05-17 08:42:41 -0600

    Add files via upload

---
+++ js/transition.js
@@ -0,0 +1,12 @@
+TweenMax.to(".overlayblue h1", 2, {
+    delay: 2,
+    opacity: 0,
+    y: 60,
+    ease: Expo.easeInOut
+  })
+  
+  TweenMax.to(".overlayblue", 2, {
+    delay: 1,
+    left: "-100%",
+    ease: Expo.easeInOut
+  })
